dc853e41c1902ef585e179fd1fdd8509ae420b95
macrob <macrob@macnew.local>
2016-12-07 12:48:38 +0200

    ref

--- back/gruntfile.js
+++ back/gruntfile.js
@@ -6,54 +6,73 @@ const debug = require('debug')('grunt:cnf:gruntfile.js');
 console.log('To show debug info set DEBUG="grunt*"');
 console.log(' set -x DEBUG \'grunt*\'');
 
-let build = path.resolve('.', 'dist', 'gbuild');
-
 const IS_BACK = true;
 
-let cnf = {
-	srcApp: path.resolve('.', 'src', 'app'),
-	root: path.resolve('.'),
-	build: build,
+const CONF_BUILD = path.resolve('.', 'dist', 'gbuild');
+const CONF_TMPL = 'html5-boilerplate';
 
-	httpPort: config.get('http.port'),
-	httpHost: config.get('http.host'),
+let wPath = '';
 
-	workingPath: ''
-};
-
-/* karma conf */
-// cnf.karma.src = 'app/';
-let tasks;
 switch (true) {
 	case process.env.wpath !== undefined:
-		cnf.workingPath = process.env.wpath;
+		wPath = process.env.wpath;
 	break;
 	case /^q?hot:app-karma/.test(process.argv[2]):
-		tasks = process.argv[2].split(':');
+		let tasks = process.argv[2].split(':');
 
 		if (tasks.length === 3) {
-				cnf.workingPath += tasks[2] + '/';
+				wPath += tasks[2] + '/';
 				process.env.wpath = tasks[2];
 		}
 	break;
 }
 
-cnf.ts = {
+process.env.karmaTestsRoot = 'app/' + wPath;
+
+let cnf = {
+	srcApp: path.resolve('.', 'src', 'app'),
+	root: path.resolve('.'),
+	workingPath: wPath,
+	build: CONF_BUILD,
+
+	httpPort: config.get('http.port'),
+	httpHost: config.get('http.host'),
+
+	workingPath: wPath,
+	ts: {
 		app: {
-				src: cnf.srcApp,
-				dest: cnf.build + '/app/'
+			src: path.resolve('.', 'src', 'app'),
+			dest: path.resolve(CONF_BUILD, 'app'),
 		},
 		appModule: {
-				src: cnf.srcApp + '/' + cnf.workingPath + '/',
-				dest: cnf.build + '/app/'
+			src: path.resolve('.', 'src', 'app', wPath),
+			dest: path.resolve(CONF_BUILD, 'app'),
 		},
 		spec: {
-				src: cnf.srcApp + '/' + cnf.workingPath + '/',
-				dest: cnf.build + '/app/',
+			src: path.resolve('.', 'src', 'app', wPath),
+			dest: path.resolve(CONF_BUILD, 'app'),
 		}
+	}
 };
 
-process.env.karmaTestsRoot = 'app/' + cnf.workingPath;
+if (!IS_BACK) {
+	cnf = Object.assign({}, cnf, {
+		srcTpl: path.resolve('.', 'src', CONF_TMPL),
+		e2eBuild: path.resolve('.', 'dist', 'e2e'),
+		e2eIsActive: /e2e/.test(process.argv[2]),
+		seleniumPort: config.get('selenium.port'),
+		seleniumHost: config.get('selenium.host'),
+		karmaPort: config.get('karma.port'),
+		karmaHost: config.get('karma.host'),
+		sass: {
+				template: {
+						src: path.resolve('.', 'src', CONF_TMPL, 'scss'),
+						dest: path.resolve(CONF_BUILD, 'css')
+				}
+		},
+	});
+}
+
 debug(cnf);
 
 module.exports = function(grunt) {
@@ -96,14 +115,51 @@ module.exports = function(grunt) {
 
 		grunt.initConfig(initConfig);
 
-		grunt.registerTask('server:background', [
-				// 'http-server:background'
-				// 'browserSync'
-				IS_BACK ? 'exec:server' : 'browserSync'
+		grunt.registerTask('hot:app', [
+				'compile:app',
+				'server:background',
+				'exec:info',
+				'todo',
+				// 'watch:app'
+				'watch'
+		]);
+
+		grunt.registerTask('qhot:app', [
+				'compile:app',
+
+				'server:background',
+				'watch:tsModule'
+		]);
+
+		grunt.registerTask('hot:app-karma', [
+				'compile:app',
+
+				/* karma */
+				'compile:spec',
+
+				'server:background',
+				'exec:info',
+				'todo',
+				// 'watch:app-karma'
+				'watch'
+		]);
+
+		grunt.registerTask('qhot:app-karma', [
+				'compile:spec',
+				'server:background',
+				'watch'
+		]);
+
+		grunt.registerTask('default', [
+			'hot:app-karma'
 		]);
 
 		switch (IS_BACK) {
 			case true:
+				grunt.registerTask('server:background', [
+						'exec:server'
+				]);
+
 				grunt.registerTask('compile:app', [
 						'tslint',
 						'clean:build',
@@ -113,6 +169,7 @@ module.exports = function(grunt) {
 						// 'compile:templateSing',
 						'ts:app',
 						'ts:appModule',
+						'replace',
 				]);
 
 				grunt.registerTask('compile:spec', [
@@ -122,6 +179,12 @@ module.exports = function(grunt) {
 				]);
 			break;
 			default:
+				grunt.registerTask('server:background', [
+						// 'http-server:background'
+						// 'browserSync'
+						'browserSync'
+				]);
+
 				grunt.registerTask('compile:app', [
 						'tslint',
 						'clean:build',
@@ -133,14 +196,16 @@ module.exports = function(grunt) {
 						// 'compile:templateSing',
 						'ts:app',
 						'ts:appModule',
-
+						'replace',
 						'exec:barrels',
 						'compile:bower'
 				]);
 
 				grunt.registerTask('compile:template', [
+						'clean:build',
 						'sass:template',
-						'copy:template'
+						'copy:template',
+						'replace'
 				]);
 
 
@@ -188,57 +253,14 @@ module.exports = function(grunt) {
 				]);
 
 				grunt.registerTask('hot:template', [
-						'clean:build',
-						'copy:template',
-						'server:background',
-						// 'watch:template'
-						'watch'
-				]);
-
-				grunt.registerTask('default', [
-						'clean:build',
+						// 'clean:build',
+						// 'copy:template',
 						'compile:template',
-						'replace',
 						'server:background',
 						// 'watch:template'
 						'watch'
 				]);
 		}
-
-		grunt.registerTask('hot:app', [
-				'compile:app',
-				'server:background',
-				'exec:info',
-				'todo',
-				// 'watch:app'
-				'watch'
-		]);
-
-		grunt.registerTask('qhot:app', [
-				'compile:app',
-
-				'server:background',
-				'watch:tsModule'
-		]);
-
-		grunt.registerTask('hot:app-karma', [
-				'compile:app',
-
-				/* karma */
-				'compile:spec',
-
-				'server:background',
-				'exec:info',
-				'todo',
-				// 'watch:app-karma'
-				'watch'
-		]);
-
-		grunt.registerTask('qhot:app-karma', [
-				'compile:spec',
-				'server:background',
-				'watch'
-		]);
 
 		grunt.loadNpmTasks('grunt-contrib-clean');
 		grunt.loadNpmTasks('grunt-contrib-copy');
